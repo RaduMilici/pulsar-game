e1b173d304014fa76dfd8534e8acd67c9e2a4d64
RaduMilici <Radu.Milici.3pg@phishme.com>
2018-09-11 17:52:02 +0300

    removed scan component

--- src/entities/character/enemy/Enemy.ts
+++ src/entities/character/enemy/Enemy.ts
@@ -1,9 +1,7 @@
 import Character from 'entities/character/Character';
 import Level from 'entities/level/Level';
 import Player from 'entities/character/player/Player';
-import GameObject from 'entities/GameObject';
 import { characterData } from 'types';
-import Scan from './Scan';
 import LookAt from './LookAt';
 
 export default class Enemy extends Character {
